19f2202ddb6bb9268f3211ab37dc1790cf6f04f8
Igor Kotrasinski <i.kotrasinsk@samsung.com>
2023-02-06 17:43:36 +0100

    Turn more functions into methods
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@samsung.com>

--- macros/src/repr/repr_struct.rs
+++ macros/src/repr/repr_struct.rs
@@ -7,43 +7,77 @@ use syn::spanned::Spanned;
 use syn::DeriveInput;
 use syn::{DataStruct, ItemStruct};
 
-fn struct_repr_name(def: &DeriveInput) -> syn::Ident {
-    format_ident!("{}Repr", def.ident)
+struct GeneratedStruct<'a> {
+    def: &'a DeriveInput,
+    struct_def: ItemStruct,
+    info: &'a ReprInfo,
 }
 
-fn repr_struct(def: &DeriveInput, e: &DataStruct, info: &ReprInfo) -> TokenStream {
-    let repr_name = struct_repr_name(def);
-    let mut struct_def = ItemStruct::from_fields(&repr_name, &e.fields.clone());
-    struct_def.convert_field_types_to_raw();
-    let repr_attr = info.underlying_type_repr_attr();
-
-    quote! {
-        #repr_attr
-        #[derive(Clone, Copy, Debug)]
-        // We're defined in a private mod. Allow re-exports.
-        pub #struct_def
+impl<'a> GeneratedStruct<'a> {
+    fn new(def: &'a DeriveInput, e: &'a DataStruct, info: &'a ReprInfo) -> Self {
+        let struct_def = ItemStruct::from_fields(&format_ident!("{}Repr", def.ident), &e.fields);
+        Self {
+            def,
+            struct_def,
+            info,
+        }
     }
-}
 
-fn impl_has_repr(def: &DeriveInput, e: &DataStruct, info: &ReprInfo) -> TokenStream {
-    let repr_name = struct_repr_name(def);
-    let impl_hasrepr = repr_impl_statement(def);
+    fn repr_type(&self) -> syn::Ident {
+        format_ident!("{}Repr", self.def.ident)
+    }
 
-    let unpack_by_value = info.packed.is_some();
-    let maybe_deref = if unpack_by_value { quote!(*) } else { quote!() };
-    let struct_def = ItemStruct::from_fields(&repr_name, &e.fields);
-    let unpacked = struct_def.unpack(!unpack_by_value, None);
-    let checks = struct_def.call_fields_raw_is_valid(!unpack_by_value);
+    fn repr_struct(&self) -> TokenStream {
+        let mut struct_def = self.struct_def.clone();
+        struct_def.convert_field_types_to_raw();
+        let repr_attr = self.info.underlying_type_repr_attr();
 
-    quote! {
-        unsafe #impl_hasrepr {
-            type Raw = #repr_name;
-            fn raw_is_valid(value: &#repr_name) -> Result<(), #CRATE::ReprError> {
-                let #unpacked = #maybe_deref value;
-                #checks
-                Ok(())
+        quote! {
+            #repr_attr
+            #[derive(Clone, Copy, Debug)]
+            // We're defined in a private mod. Allow re-exports.
+            pub #struct_def
+        }
+    }
+
+    fn impl_has_repr(&self) -> TokenStream {
+        let repr_type = self.repr_type();
+        let impl_hasrepr = repr_impl_statement(self.def);
+
+        let unpack_by_value = self.info.packed.is_some();
+        let maybe_deref = if unpack_by_value { quote!(*) } else { quote!() };
+        let struct_def = self.struct_def.clone();
+        let unpacked = struct_def.unpack(!unpack_by_value, None);
+        let checks = struct_def.call_fields_raw_is_valid(!unpack_by_value);
+
+        quote! {
+            unsafe #impl_hasrepr {
+                type Raw = #repr_type;
+                fn raw_is_valid(value: &#repr_type) -> Result<(), #CRATE::ReprError> {
+                    let #unpacked = #maybe_deref value;
+                    #checks
+                    Ok(())
+                }
             }
         }
+    }
+
+    fn repr_impl_for_struct(&self) -> syn::Result<TokenStream> {
+        if !self.info.is_c && !self.info.is_transparent {
+            return Err(syn::Error::new(
+                self.def.span(),
+                ReprDeriveError::StructNeedsReprC,
+            ));
+        }
+
+        let repr_struct = self.repr_struct();
+        let impl_hasrepr = self.impl_has_repr();
+
+        let out = quote! {
+            #repr_struct
+            #impl_hasrepr
+        };
+        Ok(out)
     }
 }
 
@@ -52,19 +86,5 @@ pub fn repr_impl_for_struct(
     e: &DataStruct,
     info: &ReprInfo,
 ) -> syn::Result<TokenStream> {
-    if !info.is_c && !info.is_transparent {
-        return Err(syn::Error::new(
-            def.span(),
-            ReprDeriveError::StructNeedsReprC,
-        ));
-    }
-
-    let repr_struct = repr_struct(def, e, info);
-    let impl_hasrepr = impl_has_repr(def, e, info);
-
-    let out = quote! {
-        #repr_struct
-        #impl_hasrepr
-    };
-    Ok(out)
+    GeneratedStruct::new(def, e, info).repr_impl_for_struct()
 }
